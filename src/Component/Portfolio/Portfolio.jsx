913b71da0d7444cb1f9ea42cd7c5c0972508d4ac
azadeh <azadeh@gmail.com>
2024-01-17 06:52:11 +0430

    Portfolio Item Created

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import './Portfolio.css'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import SideBar from '../../img/sidebar.png'
+import Ecommerce from '../../img/ecommerce.png'
+import MusicApp from '../../img/musicapp.png'
+import Hoc from '../../img/hoc.png'
+
+
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div className='portfolio'>
+      {/* heading */}
+      <span>Recent Project</span>
+      <span>Portfolio</span>
+      {/* slider */}
+      <Swiper 
+      spspaceBetween={30}
+      slidesPerView={3}
+      grabCursor={true}
+      className="portfolio-slider"
+      >
+        <SwiperSlide>
+          <img src={SideBar} alt="" />
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est libero et, reprehenderit vel consectetur, officia debitis dolorem quidem fugiat quos rerum at. Voluptas veniam officia et debitis consequuntur, corporis fugiat!</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Ecommerce} alt="" />
+        </SwiperSlide> 
+         <SwiperSlide>
+          <img src={MusicApp} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Hoc} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Hoc} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   )
 }
 
