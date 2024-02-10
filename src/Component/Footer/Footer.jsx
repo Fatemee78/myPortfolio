0259601cf9352bf3077b5db990f62b95ddb73205
fatemeh <azadeh@gmail.com>
2024-02-10 11:51:05 +0430

    changed somethings

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
+import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
-import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
 import Gitub from "@iconscout/react-unicons/icons/uil-github";
 
 const Footer = () => {
@@ -12,9 +12,15 @@ const Footer = () => {
       <div className="f-content">
         <span>azadehrezwani@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"2rem"} />
-          <Facebook color="white" size={"2rem"} />
-          <Gitub color="white" size={"2rem"} />
+          <a href="https://www.linkedin.com/in/ftmh-bakhshi/" target="_blank"  rel="noopener noreferrer">
+            <Linkedin color="white" size={"2rem"} />
+          </a>
+          <a href="https://www.instagram.com/azadeh.nfts?utm_source=qr&igsh=MzNlNGNkZWQ4Mg==" target="_blank" rel="noopener noreferrer">
+            <Insta color="white" size={"2rem"} />
+          </a>
+          <a href="https://github.com/f-Bakhshi" target="_blank" rel="noopener noreferrer">
+            <Gitub color="white" size={"2rem"} />
+          </a>
         </div>
       </div>
     </div>
